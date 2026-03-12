07925cf92cd494daa7496c7c3edab064307d2f30
yujiangan <2974539671@qq.com>
2026-03-12 13:23:06 +0800

    反馈模块优化

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -4,10 +4,6 @@ Page({
   data: {
     pageTitle: '关于',
     currentIndex: -1, // 当前展开的FAQ索引
-    showModal: false, // 反馈弹窗是否显示
-    feedbackText: '', // 用户输入的反馈内容
-
-    // FAQ列表 - 常见问题与解答
     faqList: [
       {
         question: 'AI推荐是如何工作的？',
@@ -33,74 +29,14 @@ Page({
   },
 
   // ===== 生命周期 =====
-  // 页面加载时触发
   onLoad() {
   },
 
   // ===== FAQ交互 =====
-  /**
-   * FAQ点击事件
-   * 点击展开/收起FAQ答案
-   */
   onFaqTap(e: WechatMiniprogram.TouchEvent) {
     const index = e.currentTarget.dataset.index;
-    // 如果点击的是当前展开的，则收起；否则展开新的
     this.setData({
       currentIndex: this.data.currentIndex === index ? -1 : index,
     });
   },
-
-  // ===== 反馈功能 =====
-  /**
-   * 反馈按钮点击
-   * 打开反馈弹窗
-   */
-  onFeedbackTap() {
-    this.setData({
-      showModal: true,
-      feedbackText: '', // 清空之前的内容
-    });
-  },
-
-  /**
-   * 关闭反馈弹窗
-   */
-  onCloseModal() {
-    this.setData({
-      showModal: false,
-      feedbackText: '',
-    });
-  },
-
-  /**
-   * 输入框内容变化
-   */
-  onInputChange(e: any) {
-    this.setData({
-      feedbackText: e.detail.value,
-    });
-  },
-
-  /**
-   * 提交反馈
-   * 目前只是打印日志，实际项目中可以发送到服务器
-   */
-  onSubmitFeedback() {
-    console.log('提交反馈:', this.data.feedbackText);
-
-    // 埋点：提交反馈
-    const track = require('../../utils/track.js');
-    track.track(track.EventTypes.ABOUT_FEEDBACK, {
-      contentLength: this.data.feedbackText.length
-    });
-
-    this.setData({
-      showModal: false,
-      feedbackText: '',
-    });
-  },
-
-  // ===== 工具方法 =====
-  // 阻止事件冒泡
-  stopPropagation() {}
 });
